4cadc4053d0fed5bfff39495e0aaa616b37ff37d
Enweronly <97070516@st.deltion.nl>
2021-02-03 08:33:40 +0100

    c

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,5 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('/about', [App\Http\Controllers\AboutController::class, 'index'])->name('about');
