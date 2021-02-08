5eee57771573cb852da5a22014241786aa433114
Joery Disselhof <97067740@st.deltion.nl>
2021-02-08 14:59:13 +0100

    favicon2

--- resources/views/about_us.blade.php
+++ resources/views/about_us.blade.php
@@ -8,7 +8,7 @@
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-
+        <link rel="shortcut icon" type="img\favicon.png" href="img\favicon.png"/>
         <!-- Styles -->
         <style>
             html, body {
