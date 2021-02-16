57a4bf452c30d92a81b1a4b0ce3d0e82cb82ef86
Enweronly <97070516@st.deltion.nl>
2021-02-16 09:59:24 +0100

    Web.php

--- routes/web.php
+++ routes/web.php
@@ -38,11 +38,6 @@ Route::get('/edit-car/{id}',[CarController::class,'editCar']);
 
 Route::post('/update-car',[CarController::class,'updateCar'])->name('car.update');
 
-
-
-
-
-
 Auth::routes(['verify'=>true]);
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
