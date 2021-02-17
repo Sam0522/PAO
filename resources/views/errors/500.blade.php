92ba695afd64b0c666b05099bf090e5257cec8bb
Enweronly <97070516@st.deltion.nl>
2021-02-17 10:28:34 +0100

    Error page V1

---
+++ resources/views/errors/500.blade.php
@@ -0,0 +1,115 @@
+<!DOCTYPE html>
+<html lang="NL">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Error 404</title>
+
+            <style>
+            html, body {
+                background-color: #1157d3;
+                background-image: linear-gradient(68deg, #1157d3 0%, #1048c0 17%, #0f39ad 39%, #0f299b 60%, #0e1a88 80%, #06074d 100%);
+
+                color: #ffffff;
+                font-family: 'Nunito', sans-serif;
+                font-weight: 200;
+                height: 100vh;
+                margin: 0;
+            }
+
+            .full-height {
+                height: 100vh;
+            }
+
+            .flex-center {
+                align-items: center;
+                display: flex;
+                justify-content: center;
+            }
+
+            .position-ref {
+                position: relative;
+            }
+
+            .top-right {
+                position: absolute;
+
+                top: 0px;
+                width: 100%;
+                background-color: #ffffff;
+                height: 106px;
+                /* border-bottom-left-radius: 40px;
+                border-bottom-right-radius: 25px; */
+            }
+
+            .content {
+               text-align: center;
+            }
+
+            .title {
+                font-size: 84px;
+            }
+
+            .links > a {
+                color: white;
+                padding: 0 25px;
+                font-size: 13px;
+                font-weight: 600;
+                letter-spacing: .1rem;
+                text-decoration: none;
+                text-transform: uppercase;
+            }
+
+            .m-b-md {
+                margin-bottom: 30px;
+            }
+            div{
+                font-size: 20px;
+            }
+
+            a.login{
+                float: right;
+                margin-top: 18px;
+                color: #636b6f;
+                margin-top: 35px;
+            }
+            a.login2{
+                float: left;
+                margin-top: 18px;
+                color: #636b6f;
+                margin-top: 35px;
+            }
+        </style>
+</head>
+<body>
+               <div class="content">
+                    <br><br><br><br>
+                <div class="title m-b-md">
+                Error 500
+                <br>
+                </div>
+                <div class="links">
+                    
+                </div>
+                <div>
+                    <img src="img\error500.png" height="40%">
+                </div>
+                <div>
+                <b>Unexpected error</b><br> 
+                </div>
+
+                    <br>
+
+                <div class="links">
+                    <a href=" ./">home</a>
+                    <a href="about/">About us</a>
+                    <a href="cars/" > Crud</a>
+                    <a href="https://github.com/Sam0522/PAO">GitHub</a>
+                    <a href="https://dev.azure.com/aaapie/PAO">Devops</a>
+                </div>
+            
+        </div>
+</div>
+</body>
+</html>
